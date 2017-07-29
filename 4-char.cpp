b99ea355efe2f869e392ff73e18fc54be3f9e860
Osmanyasal <osman.yasal@ogr.sakarya.edu.tr>
2017-07-29 22:08:25 +0300

    char turunu tanıdık
    char turu ve cikis karakterleri ile ilgili uygulama

---
+++ 4-char.cpp
@@ -0,0 +1,31 @@
+ // bu bolumde char veri turunu ve ekrana cikis birimlerini isledim
+ 
+ #include<iostream>
+ using namespace std;
+ int main(){
+ 	
+ 	//	char harf1 = 'A';
+ 	//	char harf2 = 'B';
+ 	//	
+ 	//	char cikis1 = '\n';	//1 satir alta in
+ 	//	char cikis2 = '\a'; // ses cikar
+	//	char cikis3 = '\t';	// 1 tab bosluk at
+	//	
+	//	cout<<harf1<<cikis1;
+	//	cout<<harf2<<endl;
+	//	cout<<cikis3<<"naber"<<cikis1;
+	//	cout<<cikis2<<endl;
+	//	
+ 	
+ 	// ----> liste <---- //
+ 	
+ 	cout<<"ad"<<'\t'<<'\t'<<"soyad"<<endl;
+ 	cout<<"---------------------"<<endl;
+ 	cout<<"Osman"<<'\t'<<'\t'<<"Yasal"<<endl;
+ 	cout<<"Buse"<<'\t'<<'\t'<<"Akkas"<<endl;
+ 	cout<<"Gozde"<<'\t'<<'\t'<<"Yakut "<<endl;
+ 	cout<<"Celal"<<'\t'<<'\t'<<"Sengor"<<endl;
+ 	
+ 	getchar();
+ 	return 0;
+ }
