407e1910624cf3f916ec9479f453b184c8a7dc2e
Osmanyasal <osman.yasal@ogr.sakarya.edu.tr>
2017-08-03 17:53:15 +0300

    Update 7-asal-sayilari-bulma.cpp

--- 7-asal-sayilari-bulma.cpp
+++ 7-asal-sayilari-bulma.cpp
@@ -4,7 +4,18 @@ using namespace std;
 
 int main() {		 
 					
-
+	int sayi = 2000;
+	bool asalmi = true;
+	for(int i=2;i<2000;i++){
+	for(int j=2;j<i;j++){
+	if(i%j==0) asalmi =false;
+	}
+		
+	if(asalmi){
+	cout<<i<<endl;
+	}
+	asalmi=true;
+	}
 
 
 
