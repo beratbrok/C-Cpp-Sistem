7c6e91503d8a4c11f82ba028b2950fe426c6cd1f
Osman Yasal <osman.yasal@ogr.sakarya.edu.tr>
2017-08-23 21:39:51 +0300

    Update Kaynak.cpp

--- sezar sifreleme/Kaynak.cpp
+++ sezar sifreleme/Kaynak.cpp
@@ -1,4 +1,4 @@
-// sezar sifreleme.
+// sezar sifreleme. Bu kodlar osman yasal tarafından yazılmıştır.
 #include<iostream>
 #include<fstream>
 #include<string> 
@@ -18,16 +18,19 @@ int main() {
 		case 1: {
 			string metin="";
 			string sifreli_metin="";
-			int iter_sayisi = rand() % 10;
+			
+			int iter_sayisi = rand() % 10;	// kac basamak kaydirilacak bilgisi rastgele olarak aliniyor.
 			system("cls");
+			
 			dosya.open("cripto metin.txt", ios::in | ios::out | ios::app);
 			if (dosya.is_open() == true) {
 				cout << "sifrelenmek istenilen metin.\n";
 				cout << "-----------\n";
 
 				cin.ignore();
-				getline(cin, metin);
+				getline(cin, metin);	// metin girisi yapiliyor
 				sifreli_metin = metin;
+				
 				for (int i = 0; i < metin.length(); i++) {		// tamami kucuk harflere cevrildi
 					if (metin[i] >= 97 && metin[i] <= 122) continue;
 					else if (metin[i] < 97 || metin[i]>122) {
@@ -53,17 +56,20 @@ int main() {
 			string sifresiz_metin;
 			int anahtar;
 			system("cls");
+			
 			cout << "dosya adi :";
-			cin.ignore();
+			cin.ignore();		// disaridan sifresi cozulecek dosyanin adi ve iterasyon bilgisi aliniyor
 			getline(cin, dosya_ad);
 			cout << "iter sayisi :"; 
 			cin >> anahtar;
+			
 			dosya.open(dosya_ad, ios::in | ios::out | ios::app);
 			if (dosya.is_open()) {
 				getline(dosya, metin);
-				metin=metin.substr(0, metin.length()-13);
+				metin=metin.substr(0, metin.length()-13);		// metindeki ...anahtar : x degeri siliniyor
 				sifresiz_metin = metin;
-				for (int i = 0; i < metin.length(); i++) {		// sifreleme bolumu
+				
+				for (int i = 0; i < metin.length(); i++) {		// sifre cozme bolumu
 					int karakter = (int)metin[i];
 					karakter -= anahtar;   
 					sifresiz_metin[i] = (char)karakter;
