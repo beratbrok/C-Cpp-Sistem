7e019a301fa53da91fc0b902d4f1b400648f649a
Osmanyasal <osman.yasal@ogr.sakarya.edu.tr>
2017-10-22 17:51:54 +0300

    biçimsel diller dfa kodlaması

---
+++ Biçimsel diller dfa kodlama/b151210083/Biçimsel diller ödev -2/Biçimsel diller ödev -2/DFA.cs
@@ -0,0 +1,109 @@
+﻿/* 
+        BİÇİMSEL DİLLER VE SOYUT MAKİNELER DERSİ 2. ÖDEV DFA PROGRAMI 
+
+ AD     : Osman
+ Soyad  : Yasal
+ NO     : B-151-210-083
+ OKUL   : Sakarya ÜNİVERSİTESİ
+ BÖLÜM  : Bilgisayar Mühendisliği
+ SINIF  : 3
+ DÖNEM  : 1
+ 
+ 
+ 
+ */
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Biçimsel_diller_ödev__2
+{
+    class DFA
+    {
+        int durum;
+        int[] kabul_durumlari;
+        string katar;
+        bool sonuc;
+
+
+        public DFA(string katar)
+        {
+            this.katar = katar;
+            kabul_durumlari = new int[] {3 , 4};
+            sonuc = true;
+            durum = 0;
+        }
+
+        public void Calistir()
+        {
+            while(katar != "")
+            {
+                if (katar.Length >= 2)  // katar uzunlugu cok fazlaysa buradan yapilir
+                {
+                    // her bir durum için a gelince nereye gidecegi belirtilmistir
+
+                    if (katar[0] == 'a' && durum == 0) { durum = 1; katar = katar.Substring(1); }
+                    else if (katar[0] == 'a' && durum == 1) { durum = 3; katar = katar.Substring(1); }
+                    else if (katar[0] == 'a' && durum == 2) { durum = 1; katar = katar.Substring(1); }
+                    else if (katar[0] == 'a' && durum == 3) { durum = 5; sonuc = false; break; }     // çöp durumunda direk bitir
+                    else if (katar[0] == 'a' && durum == 4) { durum = 3; katar = katar.Substring(1); }
+
+
+                    //  her bir durum için b gelince nereye gidecegi belirtilmistir
+                    else if (katar[0] == 'b' && durum == 0) { durum = 2; katar = katar.Substring(1); }
+                    else if (katar[0] == 'b' && durum == 1) { durum = 2; katar = katar.Substring(1); }
+                    else if (katar[0] == 'b' && durum == 2) { durum = 4; katar = katar.Substring(1); }
+                    else if (katar[0] == 'b' && durum == 3) { durum = 4; katar = katar.Substring(1); }
+                    else if (katar[0] == 'b' && durum == 4) { durum = 5; sonuc = false; break; } // çöp durumunda direk bitir
+
+                    else           // girilen ifade a veya b den baska bir ifadeyse kabul etme
+                    {
+                        durum = -1;             
+                        sonuc = false;
+                        break;
+                    }
+                }
+                else if(katar.Length < 2)   // tek karakter girilmesi durumunda (substring(1) tek karakter icin calismayacaktir. o yuzden bunu yaptim)
+                {
+
+                    if (katar[0] == 'a' && durum == 0) { durum = 1; katar = ""; }
+                    else if (katar[0] == 'a' && durum == 1) { durum = 3; katar = ""; }
+                    else if (katar[0] == 'a' && durum == 2) { durum = 1; katar = ""; }
+                    else if (katar[0] == 'a' && durum == 3) { durum = 5; sonuc = false; break; }     // çöp durumunda direk bitir
+                    else if (katar[0] == 'a' && durum == 4) { durum = 3; katar = ""; }
+
+
+                    // b gelmesi durumları ...
+                    else if (katar[0] == 'b' && durum == 0) { durum = 2; katar = ""; }
+                    else if (katar[0] == 'b' && durum == 1) { durum = 2; katar = ""; }
+                    else if (katar[0] == 'b' && durum == 2) { durum = 4; katar = ""; }
+                    else if (katar[0] == 'b' && durum == 3) { durum = 4; katar = ""; }
+                    else if (katar[0] == 'b' && durum == 4) { durum = 5; sonuc = false; break; } // çöp durumunda direk bitir
+
+                    else           // girilen ifade a veya b den baska bir ifadeyse kabul etme
+                    {
+                        durum = -1;
+                        sonuc = false;
+                        break;
+                    }
+                }
+
+            }
+
+            // son durum bizim kabul durumlarimiz icersinde yer aliyorsa , bu katari kabul et aksi halde kabul etme.
+            if (durum == kabul_durumlari[0] || durum == kabul_durumlari[1]) sonuc = true;
+            else sonuc = false;
+
+
+            Console.WriteLine("Tanınma durumu ... : " + sonuc);
+
+
+        }
+
+
+
+
+    }
+}
---
+++ Biçimsel diller dfa kodlama/b151210083/Biçimsel diller ödev -2/Biçimsel diller ödev -2/Program.cs
@@ -0,0 +1,47 @@
+﻿/* 
+        BİÇİMSEL DİLLER VE SOYUT MAKİNELER DERSİ 2. ÖDEV DFA PROGRAMI 
+
+ AD     : Osman
+ Soyad  : Yasal
+ NO     : B-151-210-083
+ OKUL   : Sakarya ÜNİVERSİTESİ
+ BÖLÜM  : Bilgisayar Mühendisliği
+ SINIF  : 3
+ DÖNEM  : 1
+ 
+ 
+ 
+ */ 
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Biçimsel_diller_ödev__2
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            string ifade;
+            string devam="e";
+
+            while (devam != "h")
+            {
+                Console.Write("DFA icin ifade giriniz :");
+                ifade = Console.ReadLine();
+                DFA deneme = new DFA(ifade.ToLower());
+                deneme.Calistir();
+
+                Console.Write("Tekrar denemek ister misiniz ... :");
+                devam = Console.ReadLine();
+                if (devam.ToLower() == "e" || devam.ToLower() == "evet") devam = devam.ToLower();
+                else devam = "h";
+
+                Console.Clear();
+            }
+            Console.WriteLine("Prosedur basari ile sonlandirildi ...");
+        }
+    }
+}
